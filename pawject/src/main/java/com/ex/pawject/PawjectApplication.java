84f1c2d7993bdea8b1944d6ed733f75107832bee
kbs019 <go824001@naver.com>
2025-07-15 22:23:12 +0900

    pawject 기본 구조 생성
    buile.gradle 과 application.properties 작업해놓았습니다. 확인 부탁 드려요

---
+++ pawject/src/main/java/com/ex/pawject/PawjectApplication.java
@@ -0,0 +1,13 @@
+package com.ex.pawject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawjectApplication.class, args);
+	}
+
+}
---
+++ pawject/src/test/java/com/ex/pawject/PawjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.pawject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
